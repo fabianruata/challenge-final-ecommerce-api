2c1be9f25e23a1130be3e764e70f2457255fe15e
fabianruata <fabianruata@gmail.com>
2025-12-19 09:19:47 -0300

    config.py

--- app/config.py
+++ app/config.py
@@ -1,16 +1,1 @@
-import os
-from dotenv import load_dotenv
-import cohere
 
-load_dotenv()
-
-COHERE_API_KEY = os.getenv("COHERE_API_KEY")
-
-if not COHERE_API_KEY:
-    raise RuntimeError("Falta definir COHERE_API_KEY en el archivo .env")
-
-co = cohere.Client(COHERE_API_KEY)
-
-EMBED_MODEL = "embed-multilingual-v3.0"
-SIMILARITY_THRESHOLD = 0.5
-MAX_HISTORY = 10
